cad501c2ecc4a08b4deb56bbbfaa257a3b1daf99
deenu-cse <153585196+deenu-cse@users.noreply.github.com>
2024-05-18 22:48:55 +0530

    f1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Nav from './nav';
 import Card from './card';
 import { Link } from 'react-router-dom';
 import Slider from './slider';
+import spider from './spiderman-10235.png'
+import marvel from './marvel-logo-34303.png'
 
 function App() {
    let [selectmovie, setselectmovie] = useState("All Movie")
@@ -43,6 +45,33 @@ function App() {
                </div>
             </div>
 
+         <footer>
+            <div className='footerbox1'>
+               <div className='footerbox2'>
+                  <div className='spider'>
+                     <img src={spider}></img>
+                  </div>
+                  <div className='company'>
+                     <h1>Company</h1>
+                     <h2>About Us</h2>
+                     <h2>About Site</h2>
+                  </div>
+                  <div className='follow'>
+                     <h1>Follow Us</h1>
+                     <h2>Instagram</h2>
+                     <h2>Github</h2>
+                  </div>
+                  <div className='contact'>
+                     <h1>Contact Us</h1>
+                     <h2>E-Mail</h2>
+                     <h2>vdeendayal866@gmail.com</h2>
+                  </div>
+               </div>
+               <div className='marvel'>
+                  <img src={marvel}></img>
+               </div>
+            </div>
+         </footer>
          </div>
       </div>
    )
